9464671630679a713995232bbc4fd08d4a047a48
EmadBinAbid <emad.bin.abid@gmail.com>
2020-05-11 00:17:18 +0530

    Added additional fields and methods.

--- include/InformationNode.h
+++ include/InformationNode.h
@@ -1,3 +1,4 @@
+#pragma once
 
 #include <string>
 
@@ -5,20 +6,30 @@ using namespace std;
 
 class InformationNode
 {
-    private:
+    protected:
         string nodeId;
         string parentId;
+        string treeId;
+        string nodeType;
+        string nodeValue;
 
     public:
         InformationNode();
-        InformationNode(string _nodeId, string _parentId);
+        InformationNode(string _nodeId, string _parentId, string _treeId, string _nodeType, string nodeValue);
         ~InformationNode();
 
         void setNodeId(string _nodeId);
+        void setNodeType(string _nodeType);
         void setParentId(string _parentId);
+        void setTreeId(string _treeId);
+        void setNodeValue(string _nodeValue);
 
         string getNodeId();
+        string getNodeType();
         string getParentId();
+        string getTreeId();
+        string getNodeValue();
 
+        virtual void compute() = 0;
         string toString();
 };
--- src/InformationNode.cpp
+++ src/InformationNode.cpp
@@ -2,14 +2,18 @@
 
 InformationNode::InformationNode()
 {
-    this->nodeId = "1";
-    this->parentId = "1";
+    this->nodeId = "";
+    this->parentId = "";
+    this->treeId = "";
+    this->nodeType = "";
 }
 
-InformationNode::InformationNode(string _nodeId, string _parentId)
+InformationNode::InformationNode(string _nodeId, string _parentId, string _treeId, string _nodeType, string _nodeValue)
 {
     this->nodeId = _nodeId;
     this->parentId = _parentId;
+    this->treeId = _treeId;
+    this->nodeType = _nodeType;
 }
 
 InformationNode::~InformationNode()
@@ -22,9 +26,24 @@ void InformationNode::setNodeId(string _nodeId)
     this->nodeId = _nodeId;
 }
 
+void InformationNode::setNodeType(string _nodeType)
+{
+    this->nodeType = _nodeType;
+}
+
 void InformationNode::setParentId(string _parentId)
 {
     this->parentId = _parentId;
+}
+
+void InformationNode::setTreeId(string _treeId)
+{
+    this->treeId = _treeId;
+}
+
+void InformationNode::setNodeValue(string _nodeValue)
+{
+    this->nodeValue = _nodeValue;
 }
 
 string InformationNode::getNodeId()
@@ -32,12 +51,30 @@ string InformationNode::getNodeId()
     return this->nodeId;
 }
 
+string InformationNode::getNodeType()
+{
+    return this->nodeType;
+}
+
 string InformationNode::getParentId()
 {
     return this->parentId;
 }
 
+string InformationNode::getTreeId()
+{
+    return this->treeId;
+}
+
+string InformationNode::getNodeValue()
+{
+    return this->nodeValue;
+}
+
 string InformationNode::toString()
 {
-    return "nodeId: " + nodeId;
+    return "{nodeId: " + this->nodeId + ", "
+    + "nodeType: " + this->nodeType + ", "
+    + "parentId: " + this->parentId + ", "
+    + "treeId: " + this->treeId + "}";
 }
