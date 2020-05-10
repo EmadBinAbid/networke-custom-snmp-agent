95bc6cddfc0c5cfb52ceb693f7ab16d0be8448fe
EmadBinAbid <emad.bin.abid@gmail.com>
2020-05-11 00:18:33 +0530

    Added program flow, constructed nodes and tree.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
 
 #include "../include/NetworkE.h"
+#include "../include/InformationTree.h"
+#include "../include/InformationNode.h"
+#include "../include/AgentObject.h"
+#include "../include/EnterpriseNumber.h"
+#include "../include/NetworkStatus.h"
+#include "../include/NetworkType.h"
+#include "../include/OrganizationIdentificationStatus.h"
+#include "../include/SecurityStatus.h"
+#include "../include/StandardOfCommunication.h"
+#include "../include/UtilityPacket.h"
+#include "../include/UtilityPort.h"
 
 using namespace std;
 
@@ -8,10 +20,59 @@ int main()
 {
 	cout << "Hello, world!" << endl;
 
-	NetworkE* afinitiNetwork = new NetworkE();
-	cout << afinitiNetwork->toString() << endl; 
+	/*
+		This version of NetworkE expects the InformationNode(s) to be added in the 
+		InformationTree(s) on the following order
+		
+		1. StandardOfCommunication
+		2. OrganizationIdentificationStatus
+		3. SecurityStatus
+		4. NetworkType
+		5. NetworkStatus
+		6. EnterpriseNumber
+		7. UtilityPort
+		8. UtilityPacket
+		9. AgentObject
+	*/
 
+	InformationNode* stdComm = 
+		new StandardOfCommunication("1", "0", "1", InformationNodeTypes::agentObject, "1");
+	InformationNode* orgIdStatus = 
+		new OrganizationIdentificationStatus("2", "0", "1", InformationNodeTypes::organizationIdentificationStatus, "3");
+	InformationNode* secStatus = 
+		new SecurityStatus("3", "0", "1", InformationNodeTypes::securityStatus, "6");
+	InformationNode* netType = 
+		new NetworkType("4", "0", "1", InformationNodeTypes::networkType, "1");
+	InformationNode* netStatus = 
+		new NetworkStatus("5", "0", "1", InformationNodeTypes::networkStatus, "4");
+	InformationNode* enterpriseNum = 
+		new EnterpriseNumber("6", "0", "1", InformationNodeTypes::enterpriseNumber, "53864");
+	InformationNode* utilPort = 
+		new UtilityPort("7", "0", "1", InformationNodeTypes::utilityPort, "8080");
+	InformationNode* utilPack = 
+		new UtilityPacket("8", "0", "1", InformationNodeTypes::utilityPacket, "1");
+	InformationNode* agentObj = 
+		new AgentObject("9", "0", "1", InformationNodeTypes::agentObject, "00");
+
+	vector<InformationNode*> informationNodesNetworkECollection;
+	informationNodesNetworkECollection.push_back(stdComm);
+	informationNodesNetworkECollection.push_back(orgIdStatus);
+	informationNodesNetworkECollection.push_back(secStatus);
+	informationNodesNetworkECollection.push_back(netType);
+	informationNodesNetworkECollection.push_back(netStatus);
+	informationNodesNetworkECollection.push_back(enterpriseNum);
+	informationNodesNetworkECollection.push_back(utilPort);
+	informationNodesNetworkECollection.push_back(utilPack);
+	informationNodesNetworkECollection.push_back(agentObj);
+
+	InformationTree* informationTree = new InformationTree("1", informationNodesNetworkECollection);
+
+	vector<InformationTree*> informationTreesNetworkECollection;
+	informationTreesNetworkECollection.push_back(informationTree);
+
+	NetworkE* networkE = new NetworkE("localhost", "Afiniti Software Solutions", informationTreesNetworkECollection);
 	
+	cout << networkE->toString() << endl; 
 
 	return 0;
 }
