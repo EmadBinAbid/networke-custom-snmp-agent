77f744fabb4942b8b77de31f5f070c6f5914451d
EmadBinAbid <emad.bin.abid@gmail.com>
2020-05-10 05:31:15 +0530

    Added raw setup file.

---
+++ setup.sh
@@ -0,0 +1,74 @@
+#########################################################################################
+#!/bin/bash
+# A script to expose three custom Object Identifiers (OIDs) for an organization's network
+# Uses SNMPD (Simple Network Management Protocol Daemon) as the server
+# Uses SNMP (Simple Network Management Protocol) to expose server commands/APIs
+#########################################################################################
+
+# Dependencies
+echo "[~] Updating packages..."
+sudo apt update
+echo "[+] Successfully updated packages"
+
+echo
+
+echo "Installing dependencies..."
+
+echo "Installing [1 of 4] - snmpd"
+sudo apt install snmpd 
+
+echo "Installing [2 of 4] - snmp"
+sudo apt install snmp 
+
+echo "Installing [3 of 4] - libsnmp-dev"
+sudo apt install libsnmp-dev
+
+echo "Installing [4 of 4] - snmp-mibs-downloader"
+sudo apt install snmp-mibs-downloader
+
+echo "[+] Successfully installed dependencies"
+
+echo
+
+# snmpd.conf
+echo "[~] Updating snmpd.conf..."
+sudo cp ./snmpd.conf /etc/snmp
+echo
+
+# Simple Network Management Protocol (SNMP) service start
+echo "[~] Starting snmpd service..."
+sudo service snmpd stop
+sudo service snmpd start
+echo "[+] Service (snmpd) started"
+
+echo
+
+#Object Idenifiers (OIDs)
+echo "[~] Fetching info from the agent..."
+echo 
+
+echo "##############"
+echo "Version Number"
+echo "##############"
+
+snmpwalk -v2c -c communitystring localhost .1.3.6.1.4.1.53864.51
+
+echo
+
+echo "###################"
+echo "Latest Signal Value"
+echo "###################"
+
+snmpwalk -v2c -c communitystring localhost .1.3.6.1.4.1.53864.52
+
+echo
+
+echo "################"
+echo "Total Disk Space"
+echo "################"
+
+snmpwalk -v2c -c communitystring localhost .1.3.6.1.4.1.53864.53
+
+echo
+
+
