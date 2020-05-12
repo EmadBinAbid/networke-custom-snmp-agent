ac319a5e4ad928d772d8f84fa3a2b3bb0d8ff1f2
EmadBinAbid <emad.bin.abid@gmail.com>
2020-05-12 23:33:15 +0530

    Added executeQuery() method.

--- src/PostgreSql.cpp
+++ src/PostgreSql.cpp
@@ -2,39 +2,41 @@
 
 bool PostgreSql::connect(string connectionString)
 {
-    char* sql;
     try
     {
-        connection C("dbname = afinitiTest user = postgres password = password \
-        hostaddr = 127.0.0.1 port = 5432");
-        if (C.is_open()) {
-            std::cout << "Opened database successfully: " << C.dbname() << endl;
-        } else {
-            std::cout << "Can't open database" << endl;
-            return 1;
-        }
-
-        /* Create SQL statement */
-        sql = "SELECT * from snmpSignals";
-
-        /* Create a non-transactional object. */
-        nontransaction N(C);
-        
-        /* Execute SQL query */
-        result R( N.exec( sql ));
-        
-        /* List down all the records */
-        for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-            cout << "id = " << c[0].as<string>() << endl;
-            cout << "signalValue = " << c[1].as<float>() << endl;
-            cout << "signalTime = " << c[2].as<string>() << endl;
-        }
-        cout << "Operation done successfully" << endl;
-        // C.disconnect ();
-        }
-        catch (const std::exception &e) 
+        this->dbConnection = new connection(connectionString);
+        if (this->dbConnection->is_open())
         {
-            cerr << e.what() << std::endl;
+            cout << "Opened database successfully: " << this->dbConnection->dbname() << endl;
             return 1;
         }
+        else
+        {
+            cout << "Can't open database" << endl;
+            return 0;
+        }
+    }
+    catch (const exception &e) 
+    {
+        cerr << e.what() << endl;
+        return 0;
+    }
+}
+
+bool PostgreSql::executeQuery(string sqlQuery, bool (*onSuccessCallback)(result))
+{
+    try
+    {
+        nontransaction nonTransactionalQuery(*(this->dbConnection));
+        result queryResult( nonTransactionalQuery.exec( sqlQuery.c_str() ));
+
+        onSuccessCallback(queryResult);
+        
+        cout << "Operation done successfully" << endl;
+    }
+    catch (const exception &e) 
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
 }
