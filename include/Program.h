c3cc35a371d527acf4067d1031f5d320a886c3b1
EmadBinAbid <emad.bin.abid@gmail.com>
2020-05-12 23:32:03 +0530

    Added private fields.

--- include/Program.h
+++ include/Program.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Program
 {
+    private:
+        string programId;
+        string programType;
+        string programPath; 
     
+    public:
+        
 };
